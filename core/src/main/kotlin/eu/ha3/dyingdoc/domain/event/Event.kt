982cfae95824dddfb62efd58b9f579421235d709
Ha3 <git@ha3.eu>
2018-03-17 22:32:20 +0100

    Expand on StatementString

--- core/src/main/kotlin/eu/ha3/dyingdoc/domain/event/Event.kt
+++ core/src/main/kotlin/eu/ha3/dyingdoc/domain/event/Event.kt
@@ -8,16 +8,38 @@ import java.util.*
  *
  * @author Ha3
  */
+
+/**
+ * Virtually represents a String that:
+ *
+ * - is not empty
+ * - is not just whitespace
+ * - has no leading whitespace nor trailing whitespace
+ */
 typealias StatementString = String
+
+private fun StatementString.asStatementString(param: String): String = this.verifyStatementString("StatementString")?.also {
+        throw IllegalArgumentException(it)
+    } ?: this
+
+/**
+ * Verifies a StatementString without throwing anything:
+ *
+ * - returns null if the StatementString is correct
+ * - otherwise returns a String representing an error message
+ */
+private fun StatementString.verifyStatementString(parameterName: String): String? = when {
+    this.isBlank() -> "$parameterName must not be blank (got: $this}"
+    (this.trim() != this) -> "$parameterName must be trimmed (got: $this}"
+    else -> null
+}
 
 sealed class Event {
     data class Request(val device: StatementString, val state: StatementString) {
         init {
             listOf(
-                device.isBlank().then("device must not be blank"),
-                (device.trim() != device).then("device must be trimmed"),
-                state.isBlank().then("state must not be blank"),
-                (state.trim() != state).then("state must be trimmed")
+                device.verifyStatementString("device"),
+                device.verifyStatementString("state")
             )
                 .filter(Objects::nonNull)
                 .apply {
